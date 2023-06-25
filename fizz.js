6f5c1ccae5b69da5db7439ea628f210fa9a7ad0b
Marcelearn <jaworskimarcel@gmail.com>
2023-06-25 14:25:50 +0200

    done

--- fizz.js
+++ fizz.js
@@ -1,15 +1,13 @@
 
 for (let i = 1; i <= 100; i ++) {
-  if (i % 3 === 0) {
-    console.log(i , "Fizz");
+  if (i % 3 == 0 && i % 5 === 0) {
+    console.log(i ,"FizzBuzz");
   }
   else if (i % 5 === 0) {
     console.log(i , "Buzz");
   }
-  else if (i % 3 == 0 && i % 5 === 0) {
-    console.log(i,"FizzBuzz");
-  } 
-  else {
-    console.log(i)
+  else if (i % 3 === 0) {
+    console.log(i , "Fizz");
   }
+  else console.log(i)
 }
